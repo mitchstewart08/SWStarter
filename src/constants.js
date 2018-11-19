ca5787f19527688a0bd778db4a13569ea0496238
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-11-18 19:10:25 -0600

    Add Color constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: "#000",
+  secondary: "#0ab463",
+  secondaryDarker: "#089954",
+  inactive: "#c4c4c4",
+  highlight: "#0094ff",
+  background: "#fff",
+  appBackground: "#ededed",
+}
