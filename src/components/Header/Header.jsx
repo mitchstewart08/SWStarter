be5635f3301459185986cc130f0c940918012603
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-11-18 18:20:32 -0600

    Add Link functionality to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import './Header.css'
 
@@ -10,7 +11,7 @@ export default class Header extends Component {
   render() {
     return (
       <div>
-        {this.props.title}
+        <Link to="/">{this.props.title}</Link>
       </div>
     )
   }
