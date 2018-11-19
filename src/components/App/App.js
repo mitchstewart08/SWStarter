08e341241d6b47a7ea763f99437e902c4108c6df
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-11-19 16:24:18 -0600

    Add Details route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Main from "../../views/Main";
 import Header from '../Header/Header.jsx'
+import Details from '../../views/Details'
 import "./App.scss";
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Header title={"SWStarter"} />
           <Switch>
             <Route exact path="/" component={Main} />
+            <Route path="/details/:name" component={Details} />
           </Switch>
         </div>
       </Router>
