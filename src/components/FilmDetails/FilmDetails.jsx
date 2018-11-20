bde1b20c7f5afe036bac29718388f20c617baa15
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-11-20 11:15:34 -0600

    Get characters but not rendering properly

--- src/components/FilmDetails/FilmDetails.jsx
+++ src/components/FilmDetails/FilmDetails.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const FilmDetails = props => {
-  const {film, characters, loading} = props
+  const {film, characters} = props
+  console.log(characters)
   return (
     <div>
+
        <div className="FilmDetails">
         <h3>{film.name}</h3>
         <div className="detailContainer">
@@ -14,16 +16,21 @@ const FilmDetails = props => {
         </div>
         <div className="detailContainer">
         <h4>Characters</h4>
+        
         <p>
+          {console.log("characthers array in film details ",characters)}
+          {console.log("characthers array in film details ",characters.length)}
+          
           {characters.map((character) => (
-            <span><Link to={{
-              pathname: `/details/${character.name}`,
-              state: {details: character}
-            }}>{character.name}</Link>,
-            </span>
+            console.log(character)
+            // <span><Link to={{
+            //   pathname: `/details/${character.name}`,
+            //   state: {details: character}
+            // }}>{character.name}</Link>,
+            // {console.log(character)}
+            // </span>
             
           ))}
-          
         </p>
         </div>
         <Link to={{pathname : '/'}}><button>Back to Search</button></Link>
--- src/views/Details.js
+++ src/views/Details.js
@@ -10,11 +10,16 @@ export default class Details extends Component {
       details : props.location.state.details,
       films: [],
       characters: [],
+      loading: true,
     }
     this.getCharacters = this.getCharacters.bind(this)
     this.getFilms = this.getFilms.bind(this)
+    this.renderContent = this.renderContent.bind(this)
 
     this.isFilm = this.state.details.name ? false : true;
+  }
+
+  componentDidMount(){
     this.isFilm ? this.getCharacters(this.state.details.characters) : this.getFilms(this.state.details.films);
   }
 
@@ -22,27 +27,39 @@ export default class Details extends Component {
     let charactersArray = [];
 
     characters.forEach((url) => {
-      console.log(url);
       axios.get(url)
            .then((res) => {
             charactersArray.push(res.data)})
            .catch((err) => console.log(err))
-           .finally(() => this.setState({characters: charactersArray}))
     })
+
+    this.setState({characters: charactersArray,
+                   loading: false});
+    console.log(this.state.loading);
   }
 
   getFilms(films){
 
   }
 
+  renderContent(){
+    if(this.state.loading){
+      return(<p>Loading</p>)
+    }else{
 
-  
+    if(this.isFilm){
+      return(<FilmDetails film={this.state.details} characters={this.state.characters} loading={this.state.loading}/>)
+    }else{
+      return(<PersonDetails person={this.state.details} films={this.state.films} loading={this.state.loading}/>)
+    }
+  }
+  }
+
   render() {
     
     return (
       <div className="box detailsBox">
-        {this.isFilm ? <FilmDetails film={this.state.details} characters={this.state.characters}/> :
-         <PersonDetails person={this.state.details} films={this.state.films} />}
+       {this.renderContent()}
       </div>
     )
   }
