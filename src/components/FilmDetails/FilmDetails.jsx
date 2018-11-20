a7e9336a5247b461e16ba9af20c8e812d982a4c7
Mitchell Stewart <mitchell.stewart08@gmail.com>
2018-11-20 08:51:41 -0600

    Basic FilmDetails

--- src/components/FilmDetails/FilmDetails.jsx
+++ src/components/FilmDetails/FilmDetails.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
-export default class FilmDetails extends Component {
-  render() {
-    const {film} = this.props;
-    console.log(film)
-    return (
-      <div>
-        Film
+const FilmDetails = props => {
+  const {film} = props
+  return (
+    <div>
+       <div className="FilmDetails">
+        <h3>{film.name}</h3>
+        <div className="detailContainer">
+          <h4>Opening Crawl</h4>
+          <p>{film.opening_crawl}</p>
+        
+        </div>
+        <div className="detailContainer">
+        <h4>Characters</h4>
+        
+        </div>
+        <Link to={{pathname : '/'}}><button>Back to Search</button></Link>
+
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default FilmDetails
+
